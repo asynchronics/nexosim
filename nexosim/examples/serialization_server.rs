00a87e7199dec9577aaa514264b971dad63956ee
maciek.glowka <maciek.glowka@gmail.com>
2025-04-30 10:17:00 +0200

    example cfg

--- nexosim/examples/serialization_server.rs
+++ nexosim/examples/serialization_server.rs
@@ -9,34 +9,38 @@ use std::time::Duration;
 
 use serde::{Deserialize, Serialize};
 
-fn bench(_: ()) -> Result<(SimInit, EndpointRegistry), SimulationError> {
+fn bench(cfg: usize) -> Result<(SimInit, EndpointRegistry), SimulationError> {
     let mut registry = EndpointRegistry::new();
 
-    let model = MyModel {
-        output: Output::default(),
-    };
-    let mbox = Mailbox::new();
-    let addr = mbox.address();
+    // let mut source = EventSource::new();
+    // source.connect(MyModel::input, &mbox);
 
-    let mut input = EventSource::new();
-    input.connect(MyModel::input, &addr);
-    registry.add_event_source(input, "input").unwrap();
+    let mut sim_init = SimInit::new().set_clock(AutoSystemClock::new());
 
-    let mut source = EventSource::new();
-    source.connect(MyModel::input, &mbox);
+    for i in 0..cfg {
+        let model = MyModel {
+            output: Output::default(),
+        };
+        let mbox = Mailbox::new();
+        let addr = mbox.address();
 
-    let mut sim_init = SimInit::new()
-        .add_model(model, mbox, "model")
-        .set_clock(AutoSystemClock::new());
+        let mut input = EventSource::new();
+        input.connect(MyModel::input, &addr);
+        registry
+            .add_event_source(input, format!("input_{}", i))
+            .unwrap();
 
-    let input_id = sim_init.register_event_source(source);
+        sim_init = sim_init.add_model(model, mbox, "model");
+    }
 
-    sim_init = sim_init
-        .with_post_init(move |_, scheduler| {
-            println!("Init");
-            scheduler.schedule_event(Duration::from_secs(5), input_id, 19);
-        })
-        .with_post_restore(|_, _| println!("Restored!"));
+    // let input_id = sim_init.register_event_source(source);
+
+    // sim_init = sim_init
+    //     .with_post_init(move |_, scheduler| {
+    //         println!("Init");
+    //         scheduler.schedule_event(Duration::from_secs(5), input_id, 19);
+    //     })
+    //     .with_post_restore(|_, _| println!("Restored!"));
 
     Ok((sim_init, registry))
 }
