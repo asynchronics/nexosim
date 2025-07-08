27ee81d8c873ab8a48e0da8f2a570b150eb43d2c
maciek.glowka <maciek.glowka@gmail.com>
2025-07-08 16:06:19 +0200

    extra file cleanup

--- nexosim/examples/serialization_mod.rs
+++
@@ -1,8 +0,0 @@
-use nexosim::time::MonotonicTime;
-
-mod serialization_macro;
-
-fn main() {
-    let t0 = MonotonicTime::EPOCH;
-    let mut simu = serialization_macro::get_bench().init(t0).unwrap();
-}
