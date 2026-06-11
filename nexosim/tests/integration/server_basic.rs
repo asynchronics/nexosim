fe4223b00a71b88b81eeca158b7506e12f2ce18f
maciek.glowka <maciek.glowka@gmail.com>
2026-06-11 13:04:02 +0200

    fix optimistic build state replace in init and restore hadlers

--- nexosim/tests/integration/server_basic.rs
+++ nexosim/tests/integration/server_basic.rs
@@ -2,14 +2,16 @@ use prost_types::Timestamp;
 use serde::{Deserialize, Serialize};
 
 use nexosim::model::{Context, Model};
-use nexosim::ports::QuerySource;
+use nexosim::ports::{EventSource, QuerySource};
 use nexosim::simulation::{Mailbox, SimInit};
 
 use super::server_utils::get_client;
 use super::server_utils::grpc_client::{
-    BuildReply, BuildRequest, Error, ErrorCode, InitReply, InitRequest, Path, ProcessQueryReply,
-    ProcessQueryRequest, StepUntilRequest, TerminateReply, TerminateRequest, build_reply,
-    init_reply, process_query_reply, step_until_request, terminate_reply,
+    BuildReply, BuildRequest, Error, ErrorCode, InitReply, InitRequest, Path, ProcessEventReply,
+    ProcessEventRequest, ProcessQueryReply, ProcessQueryRequest, RestoreReply, RestoreRequest,
+    SaveReply, SaveRequest, StepUntilRequest, TerminateReply, TerminateRequest, build_reply,
+    init_reply, process_event_reply, process_query_reply, restore_reply, save_reply,
+    simulation_client::SimulationClient, step_until_request, terminate_reply,
 };
 use crate::some_deadline_secs;
 
@@ -34,24 +36,38 @@ macro_rules! assert_resp_err {
     };
 }
 
-#[derive(Serialize, Deserialize)]
-struct SimpleModel;
+#[derive(Default, Serialize, Deserialize)]
+struct SimpleModel(i64);
 #[Model]
 impl SimpleModel {
-    async fn query(&mut self, value: i64, cx: &Context<Self>) -> i64 {
+    async fn input(&mut self, value: i64) {
+        self.0 = value;
+    }
+    async fn state_query(&mut self) -> i64 {
+        self.0
+    }
+    async fn time_query(&mut self, value: i64, cx: &Context<Self>) -> i64 {
         value * cx.time().as_secs()
     }
 }
 
 fn simple_bench(_: u8) -> Result<SimInit, Box<dyn std::error::Error>> {
-    let simple = SimpleModel;
+    let simple = SimpleModel::default();
     let simple_mbox = Mailbox::new();
 
     let mut bench = SimInit::new();
 
+    EventSource::new()
+        .connect(SimpleModel::input, &simple_mbox)
+        .bind_endpoint(&mut bench, "input")?;
+
     QuerySource::new()
-        .connect(SimpleModel::query, &simple_mbox)
-        .bind_endpoint(&mut bench, "query")?;
+        .connect(SimpleModel::state_query, &simple_mbox)
+        .bind_endpoint(&mut bench, "state_query")?;
+
+    QuerySource::new()
+        .connect(SimpleModel::time_query, &simple_mbox)
+        .bind_endpoint(&mut bench, "time_query")?;
 
     // Bench assembly.
     bench = bench.add_model(simple, simple_mbox, "simple");
@@ -70,6 +86,22 @@ async fn init_before_build_fail() {
         .unwrap();
 
     assert_resp_err!(resp, InitReply, init_reply, ErrorCode::BenchNotBuilt);
+
+    // Shutdown.
+    signal.send(()).unwrap();
+    handle.await.unwrap();
+}
+
+#[tokio::test]
+async fn restore_before_build_fail() {
+    let (mut client, signal, handle) = get_client(simple_bench).await;
+
+    // WARN This state `should` be valid. Verify on test bench change.
+    // Invalid state should not ifluence the test though.
+    let state = vec![1, 1, 14, 1, 0, 0, 0];
+    let resp = client.restore(RestoreRequest { state }).await.unwrap();
+
+    assert_resp_err!(resp, RestoreReply, restore_reply, ErrorCode::BenchNotBuilt);
 
     // Shutdown.
     signal.send(()).unwrap();
@@ -138,7 +170,7 @@ async fn build_after_init_fail() {
     let resp = client
         .process_query(ProcessQueryRequest {
             source: Some(Path {
-                segments: vec!["query".to_string()],
+                segments: vec!["time_query".to_string()],
             }),
             request: vec![3],
         })
@@ -151,6 +183,26 @@ async fn build_after_init_fail() {
         } if replies == vec![vec![9]] => (),
         a => panic!("Expected replies: [[9]], got: {:?}", a),
     };
+
+    // Shutdown.
+    signal.send(()).unwrap();
+    handle.await.unwrap();
+}
+
+#[tokio::test]
+async fn no_build_init_build() {
+    let (mut client, signal, handle) = get_client(simple_bench).await;
+
+    let resp = client
+        .init(InitRequest {
+            time: Some(Timestamp::default()),
+        })
+        .await
+        .unwrap();
+    assert_resp_err!(resp, InitReply, init_reply, ErrorCode::BenchNotBuilt);
+
+    let resp = client.build(BuildRequest { cfg: vec![0] }).await.unwrap();
+    assert_resp_ok!(resp, BuildReply, build_reply);
 
     // Shutdown.
     signal.send(()).unwrap();
@@ -194,7 +246,7 @@ async fn subsequent_init_fail() {
     let resp = client
         .process_query(ProcessQueryRequest {
             source: Some(Path {
-                segments: vec!["query".to_string()],
+                segments: vec!["time_query".to_string()],
             }),
             request: vec![3],
         })
@@ -214,7 +266,60 @@ async fn subsequent_init_fail() {
 }
 
 #[tokio::test]
-async fn build_after_complete_cycle() {
+async fn init_no_build_restore_fail() {
+    let (mut client, signal, handle) = get_client(simple_bench).await;
+
+    let resp = client.build(BuildRequest { cfg: vec![0] }).await.unwrap();
+    assert_resp_ok!(resp, BuildReply, build_reply);
+
+    // First init.
+    let resp = client
+        .init(InitRequest {
+            time: Some(Timestamp::default()),
+        })
+        .await
+        .unwrap();
+    assert_resp_ok!(resp, InitReply, init_reply);
+
+    // Step the simulation.
+    let _ = client
+        .step_until(StepUntilRequest {
+            deadline: some_deadline_secs!(2, step_until_request),
+        })
+        .await
+        .unwrap();
+
+    let state = fetch_state(&mut client).await;
+
+    // Now try restore.
+    let resp = client.restore(RestoreRequest { state }).await.unwrap();
+    assert_resp_err!(resp, RestoreReply, restore_reply, ErrorCode::BenchNotBuilt);
+
+    // Verify that the simulation is sill alive and at a right ts.
+    let resp = client
+        .process_query(ProcessQueryRequest {
+            source: Some(Path {
+                segments: vec!["time_query".to_string()],
+            }),
+            request: vec![3],
+        })
+        .await
+        .unwrap();
+    match resp.into_inner() {
+        ProcessQueryReply {
+            result: Some(process_query_reply::Result::Empty(())),
+            replies,
+        } if replies == vec![vec![6]] => (),
+        a => panic!("Expected replies: [[6]], got: {:?}", a),
+    };
+
+    // Shutdown.
+    signal.send(()).unwrap();
+    handle.await.unwrap();
+}
+
+#[tokio::test]
+async fn restore_after_complete_cycle() {
     let (mut client, signal, handle) = get_client(simple_bench).await;
 
     let resp = client.build(BuildRequest { cfg: vec![0] }).await.unwrap();
@@ -228,13 +333,59 @@ async fn build_after_complete_cycle() {
         .unwrap();
     assert_resp_ok!(resp, InitReply, init_reply);
 
+    let resp = client
+        .process_event(ProcessEventRequest {
+            source: Some(Path {
+                segments: vec!["input".to_string()],
+            }),
+            event: vec![7],
+        })
+        .await
+        .unwrap();
+    assert_resp_ok!(resp, ProcessEventReply, process_event_reply);
+
+    let state = fetch_state(&mut client).await;
+
     let resp = client.terminate(TerminateRequest {}).await.unwrap();
     assert_resp_ok!(resp, TerminateReply, terminate_reply);
 
     let resp = client.build(BuildRequest { cfg: vec![0] }).await.unwrap();
     assert_resp_ok!(resp, BuildReply, build_reply);
 
+    let resp = client.restore(RestoreRequest { state }).await.unwrap();
+    assert_resp_ok!(resp, RestoreReply, restore_reply);
+
+    // Verify restored model state
+    let resp = client
+        .process_query(ProcessQueryRequest {
+            source: Some(Path {
+                segments: vec!["state_query".to_string()],
+            }),
+            // 0xf6 decodes as `null` in cbor which can be deserialized to `()`
+            request: vec![0xf6],
+        })
+        .await
+        .unwrap();
+    match resp.into_inner() {
+        ProcessQueryReply {
+            result: Some(process_query_reply::Result::Empty(())),
+            replies,
+        } if replies == vec![vec![7]] => (),
+        a => panic!("Expected replies: [[7]], got: {:?}", a),
+    };
+
     // Shutdown.
     signal.send(()).unwrap();
     handle.await.unwrap();
+}
+
+/// Helper executing save request on the client.
+async fn fetch_state(client: &mut SimulationClient<tonic::transport::Channel>) -> Vec<u8> {
+    let resp = client.save(SaveRequest {}).await.unwrap();
+    match resp.into_inner() {
+        SaveReply {
+            result: Some(save_reply::Result::State(v)),
+        } => v,
+        a => panic!("Expected saved state, got: {:?}", a),
+    }
 }
